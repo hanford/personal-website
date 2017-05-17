4e3dd75320791c0fd311a1121a9931dbae2577c9
Jack Hanford <jackhanford@gmail.com>
2017-05-16 23:43:17 -0700

    simplify social modal

--- components/social-modal.js
+++ components/social-modal.js
@@ -3,88 +3,60 @@ import { Motion, spring, presets } from 'react-motion'
 import Link from 'next/link'
 
 export const SocialModal = ({ toggle }) => (
-  <div className='body'>
-    <section className='container'>
+  <div className='card'>
+    <button className='closeButton' onClick={toggle}>×</button>
 
-      <div className='card'>
-        <button className='closeButton' onClick={toggle}>×</button>
-
-        <ul>
-          <li>
-            <a href='https://github.com/hanford' target='_blank'>
-              <i className='icon ion-social-github'></i> Github
-            </a>
-          </li>
-          <li>
-            <a href='https://twitter.com/jackhanford' target='_blank'>
-              <i className='icon ion-social-twitter-outline'></i> Twitter
-            </a>
-          </li>
-          <li>
-            <a href='https://www.instagram.com/jackhanford/' target='_blank'>
-              <i className='icon ion-social-instagram-outline'></i> Instagram
-            </a>
-          </li>
-          <li>
-            <Link prefetch href='/snapchat'>
-              <a><i className='icon ion-social-snapchat-outline'></i> Snapchat</a>
-            </Link>
-          </li>
-          <li>
-            <a href='https://facebook.com/jackhanford' target='_blank'>
-              <i className='icon ion-social-facebook-outline'></i> Facebook
-            </a>
-          </li>
-          <li>
-            <a href='mailto:jackhanford@gmail.com' target='_blank'>
-              <i className='icon ion-ios-email-outline'></i> Email
-            </a>
-          </li>
-          <li>
-            <a href='https://www.linkedin.com/in/jack-hanford-98352761/' target='_blank'>
-              <i className='icon ion-social-linkedin-outline'></i> Linkedin
-            </a>
-          </li>
-        </ul>
-      </div>
-
-    </section>
+    <ul>
+      <li>
+        <a href='https://github.com/hanford' target='_blank'>
+          <i className='icon ion-social-github'></i> Github
+        </a>
+      </li>
+      <li>
+        <a href='https://twitter.com/jackhanford' target='_blank'>
+          <i className='icon ion-social-twitter-outline'></i> Twitter
+        </a>
+      </li>
+      <li>
+        <a href='https://www.instagram.com/jackhanford/' target='_blank'>
+          <i className='icon ion-social-instagram-outline'></i> Instagram
+        </a>
+      </li>
+      <li>
+        <Link prefetch href='/snapchat'>
+          <a><i className='icon ion-social-snapchat-outline'></i> Snapchat</a>
+        </Link>
+      </li>
+      <li>
+        <a href='https://facebook.com/jackhanford' target='_blank'>
+          <i className='icon ion-social-facebook-outline'></i> Facebook
+        </a>
+      </li>
+      <li>
+        <a href='mailto:jackhanford@gmail.com' target='_blank'>
+          <i className='icon ion-ios-email-outline'></i> Email
+        </a>
+      </li>
+      <li>
+        <a href='https://www.linkedin.com/in/jack-hanford-98352761/' target='_blank'>
+          <i className='icon ion-social-linkedin-outline'></i> Linkedin
+        </a>
+      </li>
+    </ul>
 
     <style jsx>{`
-      .body {
-        height: 100%;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .container {
-        max-width: 100%;
-        position: relative;
-      }
-
-      @media(max-width: 768px) {
-        .container {
-          width: 100%;
-          height: 100%;
-        }
-      }
-
       .card {
         box-shadow: 0 18px 35px rgba(50,50,93,.1), 0 8px 15px rgba(0,0,0,.07);
         background-color: white;
         border-radius: 0.4rem;
         padding: 2rem 4rem;
-        margin-top: 0rem;
         position: relative;
       }
 
-      @media(max-width: 768px) {
+      @media(max-width: 767px) {
         .card {
-          height: 100%;
-          margin-top: 8rem;
           padding: 2rem;
+          margin: 2rem;
         }
       }
 
@@ -151,6 +123,7 @@ export const SocialModal = ({ toggle }) => (
         transform: translateX(4rem) scale(1.15);
       }
     `}</style>
+
   </div>
 )
 
