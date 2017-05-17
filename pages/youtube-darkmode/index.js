528196227ade1107fe29719504bb92bffdf08000
Jack Hanford <jackhanford@gmail.com>
2017-05-16 22:06:38 -0700

    add video section to youtube-dark

--- pages/youtube-darkmode/index.js
+++ pages/youtube-darkmode/index.js
@@ -13,10 +13,13 @@ export default () => (
 
     <div className='hero'>
       <div className='content'>
-        <h1 style={{marginBottom: '0'}}>Youtube Material UI</h1>
-        <p style={{marginTop: '0.5rem', marginBottom: '3rem'}}>Google is redesigning youtube and adding darkmode, this enables it right now.</p>
+        <h1 style={{marginBottom: '0'}}>Youtube Darkmode <span style={{fontSize: '3rem'}}>🎉</span></h1>
+        <p style={{marginTop: '0.5rem', marginBottom: '3rem'}}>
+          Google is redesigning youtube and also adding darkmode, this enables it right now
+        </p>
 
-        <iframe width='100%' height='360' src='https://www.youtube.com/embed/plz6bGOb3VI' frameborder='0' allowfullscreen></iframe>
+        <img src='../static/darkmode.png' />
+
 
         <a
           target='_blank'
@@ -25,6 +28,14 @@ export default () => (
         >
           Add to Google Chrome
         </a>
+
+
+        <h2 style={{marginTop: '8rem'}}>How to video</h2>
+        <p style={{maxWidth: '60rem', margin: '0 auto'}}>After installing the chrome extension, simply go to youtube and click "ok", and you'll load up the new youtube interface, from there you can enable darkmode or just enjoy the new white interface.</p>
+        <br />
+        <iframe width='100%' height='360' src='https://www.youtube.com/embed/plz6bGOb3VI' frameborder='0' allowfullscreen></iframe>
+        <br />
+        <br />
       </div>
     </div>
 
@@ -32,7 +43,6 @@ export default () => (
       .container {
         display: flex;
         max-width: 100%;
-        height: 100%;
         justify-content: center;
         text-align: center;
       }
@@ -44,6 +54,7 @@ export default () => (
         color: rgba(0, 0, 0, 0.8);
         margin: 6rem 0;
         min-height: 60rem;
+        padding: 2rem;
       }
 
       .content {
@@ -73,9 +84,10 @@ export default () => (
         font-weight: bold;
       }
 
-      iframe {
-        width: 100%;
-        max-width: 60rem;
+      iframe,
+      img {
+        width: 60rem;
+        max-width: 80%;
         box-sizing: border-box;
         margin: 0 auto;
         box-shadow: 0 0.4rem 4rem rgba(0, 0, 0, 0.35);
