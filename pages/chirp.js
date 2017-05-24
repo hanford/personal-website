ea5fb2cef4e39493473d8db1d2a688ee888603a2
Jack Hanford <jackhanford@gmail.com>
2017-05-24 13:21:29 -0700

    update chirp links to point to 0.3.0

--- pages/chirp.js
+++ pages/chirp.js
@@ -26,7 +26,7 @@ export default () => (
           <div className='download-row'>
             <a
               target='_blank'
-              href='https://file-skihftbnli.now.sh/Chirp-darwin-x64.zip'
+              href='https://file-youbqvkfrz.now.sh/Chirp-darwin-x64.zip'
               className='backButton'
               download='Chirp.zip'
             >
@@ -35,7 +35,7 @@ export default () => (
 
             <a
               target='_blank'
-              href='https://file-bxdmzaqkor.now.sh/Chirp-linux-x64.zip'
+              href='https://file-dtxexrxpvf.now.sh/Chirp-linux-x64.zip'
               className='backButton'
               download='Chirp.zip'
             >
@@ -44,7 +44,7 @@ export default () => (
 
             <a
               target='_blank'
-              href='https://file-tfmmrxtwrd.now.sh/Chirp-win32-x64.zip'
+              href='https://file-tntnkastlp.now.sh/Chirp-win32-x64.zip'
               className='backButton'
               download='Chirp.zip'
             >
