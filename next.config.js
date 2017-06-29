6deb29e706fe5481e3fce8855161232c210d075e
Jack Hanford <jackhanford@gmail.com>
2017-06-29 12:45:47 -0700

    migrate to preact in prod!

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  webpack: function (config, { dev }) {
+    // For the development version, we'll use React.
+    // Because, it support react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
+    config.resolve.alias = {
+      'react': 'preact-compat/dist/preact-compat',
+      'react-dom': 'preact-compat/dist/preact-compat',
+      'react-motion': 'preact-motion'
+    }
+
+    return config
+  }
+}
