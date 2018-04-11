ab7e490ec896cac55daf546c15d8af17d4c5316c
hanford <jackhanford@gmail.com>
2018-04-11 00:18:35 -0700

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,11 @@ export default class DocumentComponent extends Document {
         <Head>
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
 
-          <link rel="manifest" href="/static/manifest.json" />
+          <link rel='apple-touch-icon' sizes='180x180' href='/static/apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='/static/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/static/favicon-16x16.png' />
+
+          <link rel='manifest' href='/static/manifest.json' />
         </Head>
         <body>
           <Main />
