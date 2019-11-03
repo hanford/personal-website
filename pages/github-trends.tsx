7566bbc91c82f97222e9a2704acbe62ca692108f
Jack Hanford <jackhanford@gmail.com>
2019-11-03 00:09:03 -0700

    add cross platform copy to github trends page

--- pages/github-trends.tsx
+++ pages/github-trends.tsx
@@ -7,21 +7,45 @@ function Snapchat() {
   return (
     <Container>
       <Head title="Github Trends | Jack Hanford" />
-      <Content>
+      <Page width="700">
         <div>
           <Icon src="../static/github-trends.png"  />
           <Heading>Github Trends</Heading>
-          <p>Mac OS and iOS app for browsing trending github repos</p>
+          <P>Mac OS and iOS app for browsing trending github repos</P>
         </div>
 
         <div>
           <LightPhone src="../static/trends-light.png" />
           <DarkPhone src="../static/trends-dark.png" />
         </div>
-      </Content>
+      </Page>
+
+      <Page width="1100">
+        <div>
+          <LightMac src="../static/trends-light-mac.png" />
+          <DarkMac src="../static/trends-dark-mac.png" />
+        </div>
+
+        <PlatformCopy>
+          <Heading>Desktop and mobile</Heading>
+          <P>Github trends is written with Swift UI, so it's code runs on all apple devices</P>
+        </PlatformCopy>
+      </Page>
     </Container>
   );
 }
+
+const P = styled.p`
+  line-height: 24px;
+`
+
+const PlatformCopy = styled.div`
+  padding-left: 100px;
+
+  @media(max-width: 800px) {
+    padding-left: 0;
+  }
+`
 
 const Icon = styled.img`
   height: 7rem;
@@ -39,7 +63,7 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: row;
+  flex-direction: column;
   max-width: 100%;
   min-height: 100vh;
   padding: 16px;
@@ -55,13 +79,15 @@ const Container = styled.div`
   }
 `;
 
-const Content = styled.div`
+const Page = styled.div`
   max-width: 100%;
-  width: 70rem;
+  width: ${({ width }: any) => width}px;
   font-family: -apple-system, BlinkMacSystemFont, sans-serif;
   display: flex;
   flex-direction: row;
   align-items: center;
+  min-height: 100vh;
+
   @media(max-width: 800px) {
     flex-direction: column;
   }
@@ -100,7 +126,41 @@ const DarkPhone = styled.img`
   @media (prefers-color-scheme: light) {
     display: none;
   }
+`
 
+const LightMac = styled.img`
+  margin: 0 0 0 36px;
+  max-width: 600px;
+  width: 100%;
+
+  @media(max-width: 800px) {
+    margin: 36px auto;
+  }
+
+  @media(max-width: 800px) {
+    flex-direction: column;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    display: none;
+  }
+`
+const DarkMac = styled.img`
+  margin: 0 0 0 36px;
+  max-width: 600px;
+  width: 100%;
+
+  @media(max-width: 800px) {
+    margin: 36px auto;
+  }
+
+  @media(max-width: 800px) {
+    flex-direction: column;
+  }
+
+  @media (prefers-color-scheme: light) {
+    display: none;
+  }
 `
 
 export default withSegment(Snapchat);
