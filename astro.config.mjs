80b186d03eedd70f5490ebc6f0771aac21ae8a27
Jack Hanford <jackhanford@gmail.com>
2025-09-17 15:03:40 -0700

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
